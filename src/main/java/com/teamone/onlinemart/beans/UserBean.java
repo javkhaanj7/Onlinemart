cb5a59df261261f74868a1b378dfb02bca41584d
Javkhlant <javkhlant.ch@gmail.com>
2015-04-05 19:45:26 -0500

    Merge origin/master
    Conflicts:
    	Desktop/Bingo/web/resources/css/styles.css

--- src/main/java/com/teamone/onlinemart/beans/UserBean.java
+++ src/main/java/com/teamone/onlinemart/beans/UserBean.java
@@ -7,8 +7,13 @@ package com.teamone.onlinemart.beans;
 
 import com.teamone.onlinemart.dao.UserDAO;
 import java.io.Serializable;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.component.UIInput;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ComponentSystemEvent;
 
 /**
  *
@@ -29,9 +34,35 @@ public class UserBean implements Serializable {
     public UserBean() {
     }
     
-    public String createUser (){
+    public String register(){
         UserDAO.create(firstname, lastname, username, email, phone, password);
         return "profile?faces-redirect=true";
+    }
+    
+    public void validatePassword(ComponentSystemEvent event) {
+        FacesContext fc = FacesContext.getCurrentInstance();
+        UIComponent components = event.getComponent();
+        
+        // get password
+        UIInput uiInputPassword = (UIInput) components.findComponent("password");
+        String password = uiInputPassword.getLocalValue() == null ? "" : uiInputPassword.getLocalValue().toString();
+        String passwordId = uiInputPassword.getClientId();
+        
+        // get confirm password
+        UIInput uiInputConfirmPassword = (UIInput) components.findComponent("confirmPassword");
+        String confirmPassword = uiInputConfirmPassword.getLocalValue() == null ? "" : uiInputConfirmPassword.getLocalValue().toString();
+        
+        // Let required="true" do its job
+        if(password.isEmpty() || confirmPassword.isEmpty()) {
+            return;
+        }
+        
+        if(!password.equals(confirmPassword)) {
+            FacesMessage msg = new FacesMessage("Password must match confirm password.");
+            msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+            fc.addMessage(passwordId, msg);
+            fc.renderResponse();
+        }
     }
     
     /**
