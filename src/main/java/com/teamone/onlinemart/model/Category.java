baf52aa8d51ae1ced7db6aee1a574f6e29496bb8
javkhaa_j7 <javkhaanj7@gmail.com>
2015-04-05 22:44:16 -0500

    Category progress

---
+++ src/main/java/com/teamone/onlinemart/model/Category.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.teamone.onlinemart.model;
+
+/**
+ *
+ * @author javkhaa_j7
+ */
+public class Category {
+    
+    private int id;
+    private int parentId;
+    private String parentName;
+    private String name;
+    private String description;
+
+    public Category(int id, int parentId, String parentName, String name, String description) {
+        this.id = id;
+        this.parentId = parentId;
+        this.parentName = parentName;
+        this.name = name;
+        this.description = description;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the parentId
+     */
+    public int getParentId() {
+        return parentId;
+    }
+
+    /**
+     * @param parentId the parentId to set
+     */
+    public void setParentId(int parentId) {
+        this.parentId = parentId;
+    }
+
+    /**
+     * @return the parentName
+     */
+    public String getParentName() {
+        return parentName;
+    }
+
+    /**
+     * @param parentName the parentName to set
+     */
+    public void setParentName(String parentName) {
+        this.parentName = parentName;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
