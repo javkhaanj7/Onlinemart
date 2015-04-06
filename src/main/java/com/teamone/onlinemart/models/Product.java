56471e37bf64e149e9c4a4a9525fcd874d0da0e5
Ichko <Ichko@10.10.13.7>
2015-04-06 14:16:22 -0500

    #Product model

---
+++ src/main/java/com/teamone/onlinemart/models/Product.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.teamone.onlinemart.models;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Ichko
+ */
+public class Product implements Serializable{
+    private int id;
+    private String name;
+    private String description;
+    private double price;
+    private int category_id;
+    private int vendor_id;
+
+    public Product(){
+        
+    }
+    public Product(String name, String desc, double price){
+        this.name = name;
+        this.description = desc;
+        this.price = price;
+    }
+    public Product(int id, String name, String description, double price) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getCategory_id() {
+        return category_id;
+    }
+
+    public void setCategory_id(int category_id) {
+        this.category_id = category_id;
+    }
+
+    public int getVendor_id() {
+        return vendor_id;
+    }
+
+    public void setVendor_id(int vendor_id) {
+        this.vendor_id = vendor_id;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" + "id=" + id + ", name=" + name + ", description=" + description + ", price=" + price + ", category_id=" + category_id + ", vendor_id=" + vendor_id + '}';
+    }
+    
+    
+}
