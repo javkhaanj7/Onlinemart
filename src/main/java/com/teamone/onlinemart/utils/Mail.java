edb5e71f8d9f24aff99b6b0a543abd4182908d07
javkhaa_j7 <javkhaanj7@gmail.com>
2015-04-11 03:04:07 -0500

    Send email function added

---
+++ src/main/java/com/teamone/onlinemart/utils/Mail.java
@@ -0,0 +1,58 @@
+package com.teamone.onlinemart.utils;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+/**
+ *
+ * @author javkhaa_j7
+ */
+public class Mail {
+
+    Properties props = new Properties();
+    Session session = Session.getDefaultInstance(props,
+            new javax.mail.Authenticator() {
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication("onlinemart.shopping", "onlinemartpass");
+                }
+            });
+
+    public Mail() {
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.socketFactory.port", "465");
+        props.put("mail.smtp.socketFactory.class",
+                "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.port", "465");
+    }
+
+    public void sendEmail(String from, String to, String subject, String msg) {
+        try {
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(from));
+            message.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse(to));
+            message.setSubject(subject);
+            message.setText(msg);
+
+            Transport.send(message);
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+   
+}
